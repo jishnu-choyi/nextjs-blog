89d69c36c42b9d398a7e71394b0c6b6cd9112b2e
Jishnu Choyi <c.jishnu@gmail.com>
2023-07-12 14:15:54 +0530

    fix date error

--- pages/components/date.js
+++ pages/components/date.js
@@ -1,6 +1,10 @@
 import { parseISO, format } from "date-fns";
 
 export default function Date({ dateString }) {
-    const date = parseISO(dateString);
-    return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>;
+    const date = dateString ? parseISO(dateString) : "";
+    return date ? (
+        <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>
+    ) : (
+        <time>{"--"}</time>
+    );
 }
